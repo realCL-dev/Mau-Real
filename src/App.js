8262c1fd214074c299856cca4398b3f91d607649
clarel <cclarel@gmail.com>
2025-05-30 18:12:24 +0400

    4. Completed the Tab for the UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 
-function App() {
+import Navigation from './components/Navigation'
+import RealEstate from './abis/RealEstate.json'
+import escrow from './abis/Escrow.json'
+import config from './config.json'
+
+function App () {
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+      })
+      const account = ethers.getAddress(accounts[0])
+      setAccount(account)
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <div>
+        <h3>Welcome to Mau-Real</h3>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
